3c9ab6d6ffd761c4d2f0f4ca911dd6d2b95230c0
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-02-14 03:15:36 +0000

    layout fix

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import Map from '../components/Map';
+import Head from "next/head";
+import Map from "./Map";
 
-export default function Container ({children}) {
-    return(
-        <div>
-            <div>
-                <Map /> 
-            </div>
-            <div>
-                {children}
-            </div>
-        </div>
-    )
+export default function Container({ children }) {
+  return (
+    <div className="flex w-screen h-screen">
+      <Head>
+        <title>storyarc</title>
+        <meta
+          name="description"
+          content="O storyarc é uma plataforma de arquivo de conteúdos que pressupõe a partilha e consulta de informações relativas a espaços e à sua evolução ao longo dos anos, de modo a preservar a memória patrimonial do distrito de Aveiro."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="w-9/12">
+        <Map />
+      </div>
+      <div className="w-3/12">{children}</div>
+    </div>
+  );
 }
