698efb157cac1883bd2e2fbc8d97b19fc74afc62
teixeiraj25 <joao.teixeira04@ua.pt>
2022-02-15 17:01:32 +0000

    alteração header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ function Header () {
     return (
         <div>
             <img className="w-32 ml-3 mt-6 cursor-pointer" src='./images/logo.png'></img>
-            <h1>piroca</h1>
         </div>
     )
 }
