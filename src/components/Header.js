a5cd2d725bbf57dd8464b4fccebbfe335d4f88e2
teixeiraj25 <joao.teixeira04@ua.pt>
2022-02-15 18:33:28 +0000

    searchbar e tipo de letra

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import logo from '../../public/images/logo.png';
 function Header () {
     return (
         <div>
-            <img className="w-32 ml-3 mt-6 cursor-pointer" src='./images/logo.png'></img>
+            <img className="w-28 ml-3 mt-7 cursor-pointer" src='./images/logo.png'></img>
         </div>
     )
 }
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+const Search = () => (
+    <form action="/" method="get">
+        <input className="text-sm border rounded border-gray-200 mt-6 ml-3 p-2 pl-2 md:w-60"
+            type="text"
+            id="header-search"
+            placeholder="Pesquise por locais"
+            name="s" 
+        />
+    </form>
+);
+
+export default Search;
