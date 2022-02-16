93dbd2c4f598e852aa15893b5992027986d8e7a0
David Amaral Silva <david_silva_94@live.com.pt>
2022-02-16 19:15:30 +0000

    Next.config.js
    Removi uma API de lá

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import React from 'react';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 const Search = () => (
-  <div  className="text-sm border rounded border-gray-200 mt-6 ml-3 p-2 pl-2 sm:w-80 md:w-60">  
+  <div className="text-sm border rounded border-gray-200 mt-6 ml-3 p-2 pl-2 sm:w-80 md:w-60">  
     <GooglePlacesAutocomplete 
       apiKey="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMY8_V3zDNFubRe3vjsiiduWIhLk4oUrg&libraries=places"
       apiOptions={{ language: 'pt', region: 'pt' }}
@@ -11,19 +11,3 @@ const Search = () => (
 );
 
 export default Search;
-
-
-/*
-
-const Search = () => (
-    <form action="/" method="get">
-        <input className="text-sm border rounded border-gray-200 mt-6 ml-3 p-2 pl-2 sm:w-80 md:w-60"
-            type="text"
-            id="header-search"
-            placeholder="Pesquise por locais"
-            name="s" 
-        />
-    </form>
-);
-
-*/
