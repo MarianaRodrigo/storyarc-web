84fdf47cfd8385d9d6955fed699d9c2e9e382b9e
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-02-16 23:19:19 +0000

    api correta

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
-const Search = () => (
-  <div className="text-sm border rounded border-gray-200 mt-6 ml-3 p-2 pl-2 sm:w-80 md:w-60">  
-    <GooglePlacesAutocomplete 
-      apiKey="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMY8_V3zDNFubRe3vjsiiduWIhLk4oUrg&libraries=places"
-      apiOptions={{ language: 'pt', region: 'pt' }}
-    />
-  </div>
-);
-
-export default Search;
+export default function Search() {
+  const [value, setValue] = useState(null);
+  console.log(value);
+  return (
+    <div className="text-sm border rounded border-gray-200 mt-6 ml-3 p-2 pl-2 sm:w-80 md:w-60">
+      <GooglePlacesAutocomplete
+        selectProps={{
+          value,
+          onChange: setValue,
+        }}
+        apiKey="AIzaSyBMY8_V3zDNFubRe3vjsiiduWIhLk4oUrg"
+        apiOptions={{ language: "pt", region: "pt" }}
+        debounce={400}
+        autocompletionRequest={{
+          bounds: [
+            { lat: 50, lng: 50 },
+            { lat: 100, lng: 100 },
+          ],
+          componentRestrictions: {
+            country: ["pt"],
+          },
+        }}
+        minLengthAutocomplete={3}
+      />
+    </div>
+  );
+}
