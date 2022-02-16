57692c33a527051aea89190fdea186510b90a4b6
David Amaral Silva <david_silva_94@live.com.pt>
2022-02-16 16:52:50 +0000

    Update Header.js
    Meti o logo responsivo e da forma correta porque a maneira como se exportou o logo foi de maneira errada.  e cenas de tailwind para criar o header ficar bem formatado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
-import logo from '../../public/images/logo.png';
+import Image from 'next/image'
 
 function Header () {
     return (
-        <div>
-            <img className="w-28 ml-3 mt-7 cursor-pointer" src='./images/logo.png'></img>
-        </div>
+        <header className="Sticky top-0 z-50 gird grid-cols-2 -md p-5 md:px-10 hidden:md-inline" >
+            <div className="flex items-center space-x-4 justify-end">
+                <Image className="h-6 w-6"
+                    src='/images/logo.png' 
+                    alt="Storyarc_logo" 
+                    width="123"
+                    height="29"
+                    layout="fixed"
+                    objectFit="containt"
+                    objectPosition="Right"     
+                /> 
+            </div>
+        </header>
     )
 }
 
