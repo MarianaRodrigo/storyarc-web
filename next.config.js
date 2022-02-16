46ec4310a713880544702e1750d7571a74cf44eb
David Amaral Silva <david_silva_94@live.com.pt>
2022-02-16 19:11:05 +0000

    Search bar
    A Search bar dá fetch mas fica no loading

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
   env: {
     mapbox_key:
       "pk.eyJ1IjoiamFyYmFzNzciLCJhIjoiY2t6bGp2MjY5MmdtdDJub2NmYXR6ZWE3ZiJ9.7tPF-rPpXiE99xWJUVKBbg",
+    
+    googleLoc_key:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBMY8_V3zDNFubRe3vjsiiduWIhLk4oUrg&libraries=places"
   },
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+
+const Search = () => (
+  <div  className="text-sm border rounded border-gray-200 mt-6 ml-3 p-2 pl-2 sm:w-80 md:w-60">  
+    <GooglePlacesAutocomplete 
+      apiKey="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMY8_V3zDNFubRe3vjsiiduWIhLk4oUrg&libraries=places"
+      apiOptions={{ language: 'pt', region: 'pt' }}
+    />
+  </div>
+);
+
+export default Search;
+
+
+/*
+
 const Search = () => (
     <form action="/" method="get">
         <input className="text-sm border rounded border-gray-200 mt-6 ml-3 p-2 pl-2 sm:w-80 md:w-60"
@@ -9,4 +26,4 @@ const Search = () => (
     </form>
 );
 
-export default Search;
+*/
