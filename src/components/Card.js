fa44aabb1157c5b89d9cbf0e277db4f2fc75cdf1
David Amaral Silva <david_silva_94@live.com.pt>
2022-02-18 00:33:58 +0000

    card.js
    nao passa a data corretamente

--- src/components/Card.js
+++ src/components/Card.js
@@ -17,7 +17,7 @@ function Card({post}) {
                     <img img key={post.id} className="w-9 h-9 rounded-full mr-4" src={post.photo} alt={post.altimg}/>
                     <div>
                         <p key={post.id} className="text-black text-sm leading-none mt-1">{post.userId}</p>
-                        <p key={post.id} className="text-gray-400 text-xs mt-1">{post.postDate}</p>
+                        <p key={post.id} className="text-gray-400 text-xs mt-1">{post.createdAt}</p>
                     </div>
                 </div>
             </div>
