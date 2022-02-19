c3905862f7a65d093eb7dd68064fdd3f3ef897e4
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-02-19 18:27:45 +0000

    Updated Feed Cointainer height

--- src/components/FeedContainer.js
+++ src/components/FeedContainer.js
@@ -1,7 +1,3 @@
 export default function FeedContainer({ children }) {
-  return (
-    <div className="w-full max-h-[80%] overflow-y-scroll space-y-4 pt-6">
-      {children}
-    </div>
-  );
+  return <div className="w-full h-[81%] space-y-4 pt-6">{children}</div>;
 }
