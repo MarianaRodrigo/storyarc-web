e37798aab8987c26cd90e1d046b546e128264336
David Amaral Silva <david_silva_94@live.com.pt>
2022-02-19 17:24:38 +0000

    MapboxMap.js
    Pin added

--- src/components/MapBoxMap.js
+++ src/components/MapBoxMap.js
@@ -1,4 +1,5 @@
-import Map from "react-map-gl";
+import Map,{Marker} from "react-map-gl";
+
 
 export default function MapBoxMap() {
   return (
@@ -8,10 +9,17 @@ export default function MapBoxMap() {
         longitude: -8.656528,
         latitude: 40.62899,
         zoom: 15.31,
+
       }}
       style={{ width: "100%", height: "100%" }}
       mapStyle="mapbox://styles/mapbox/outdoors-v11"
       logoPosition="bottom-right"
-    />
+      >
+
+    <Marker longitude={-8.656528} latitude={40.62899}  >
+      <img src="/images/Marker.png" alt="Marker map png"/>
+    </Marker>
+      
+    </Map>
   );
 }
