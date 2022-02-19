219ce32fbc478efe23f668a0f34e7151ed0dc2da
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-02-19 20:52:39 +0000

    removed h-full from FeedContainer

--- src/components/FeedContainer.js
+++ src/components/FeedContainer.js
@@ -1,6 +1,6 @@
 export default function FeedContainer({ children }) {
   return (
-    <div className="flex-1 w-full h-full space-y-4 mt-6 pb-20 overflow-y-scroll overscroll-y-contain scroll-smooth">
+    <div className="flex-1 w-full space-y-4 mt-6 pb-6 overflow-y-scroll overscroll-y-contain scroll-smooth">
       {children}
     </div>
   );
