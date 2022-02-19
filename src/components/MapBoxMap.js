b4041a6cf853ca53f8b8ae4c899e55aa46bc307f
David Amaral Silva <david_silva_94@live.com.pt>
2022-02-19 18:42:50 +0000

    Update Mapbox.js
    Marker

--- src/components/MapBoxMap.js
+++ src/components/MapBoxMap.js
@@ -1,7 +1,13 @@
-import Map,{Marker} from "react-map-gl";
+import Map, {Marker} from "react-map-gl";
+import db from "../../db.json";
+
 
 
 export default function MapBoxMap() {
+
+  const posts = db.posts;
+
+
   return (
     <Map
       mapboxAccessToken={process.env.mapbox_key}
@@ -9,17 +15,26 @@ export default function MapBoxMap() {
         longitude: -8.656528,
         latitude: 40.62899,
         zoom: 15.31,
-
       }}
       style={{ width: "100%", height: "100%" }}
       mapStyle="mapbox://styles/mapbox/outdoors-v11"
       logoPosition="bottom-right"
       >
-
-    <Marker longitude={-8.656528} latitude={40.62899}  >
-      <img src="/images/Marker.png" alt="Marker map png"/>
-    </Marker>
-      
+    {posts.map((post) => (
+      <Marker 
+          longitude={-8.656528} 
+          latitude={40.62899}
+          id={`${post.id}`}
+          key={post.id}
+          onSelected={() =>
+            navigation.navigate("Post", {
+              content: post,
+            })
+          }
+          coordinate={[post.location[1], post.location[0]]}>
+        <img src="/images/Marker.png" alt="Marker map png"/>
+      </Marker> 
+      ))}
     </Map>
   );
 }
