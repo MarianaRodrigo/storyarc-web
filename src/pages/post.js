e423187e777bb3a6bf6bea03882f75f3317dd877
teixeiraj25 <joao.teixeira04@ua.pt>
2022-02-20 19:14:51 +0000

    header do post dos respetivos pins

--- src/pages/post.js
+++ src/pages/post.js
@@ -2,6 +2,8 @@ import { useRouter } from "next/router";
 import db from "../../db.json";
 import Card from "../components/Card";
 import FeedContainer from "../components/FeedContainer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 export default function Post() {
   const router = useRouter();
@@ -12,7 +14,13 @@ export default function Post() {
 
   return (
     <div className="flex-1">
-      <h1>Publicações na {router.query.rua}</h1>
+      <div className="mx-4 my-6 leading-loose">
+        <h1 className="text-base font-light tracking-wide pb-2">Publicações em</h1>
+        <div className="flex">
+          <FontAwesomeIcon className="w-5" icon={faLocationDot} />
+          <h1 className="text-xl tracking-wide font-medium px-2">{router.query.rua}</h1>
+        </div>
+      </div>
       <FeedContainer>
         {matchedPosts.map((post) => (
           <Card key={post.id} post={post} />
