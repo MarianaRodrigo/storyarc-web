804167777d7d76a8b6301781f379dc100ff4c0ec
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-02-20 16:11:25 +0000

    Feed Container update

--- src/components/FeedContainer.js
+++ src/components/FeedContainer.js
@@ -1,6 +1,6 @@
 export default function FeedContainer({ children }) {
   return (
-    <div className="flex-1 w-full space-y-4 pb-6 overflow-y-scroll overscroll-y-contain scroll-smooth">
+    <div className="flex-1 w-full h-full space-y-4 pb-6 overflow-y-scroll overscroll-y-contain scroll-smooth">
       {children}
     </div>
   );
