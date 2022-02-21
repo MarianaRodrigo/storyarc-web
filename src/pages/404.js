1b92694ce7906bf768a16ee266e6a56750f308b6
teixeiraj25 <joao.teixeira04@ua.pt>
2022-02-21 18:25:34 +0000

    página 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,17 @@
 export default function Custom404() {
-    return (
-        <div>
-        <h1>Pareces estar perdido na Historia</h1>
-            <img src=""  alt="ampulheta erro 404"/>
-            
-            <h3>Clica</h3>
-            
-            <button type="button"> aqui </button>
-             
-            <h3>para voltares ao presente</h3>
-        </div> 
-    )
-  }
+  return (
+    <div className="flex-1">
+      <div className="flex flex-col items-center">
+        <h1 className="flex justify-center font-semibold tracking-widest text-xl" style={{ color: "#37b780" }}>Pareces estar perdido na historia</h1>
+        <img className="flex m-auto w-64 mt-6" src="./images/ampulheta.png" alt="ampulheta erro 404" />
+        <div className="flex justify-center mt-6">
+          <h3 className="font-normal tracking-widest mr-2 text-gray-500">Clica </h3>
+
+          <button className="rounded px-5 py-1 tracking-widest font-semibold" style={{ backgroundColor: "#37b780", color: "white" }} type="button"> aqui </button>
+
+          <h3 className="font-normal tracking-widest ml-2 text-gray-500">para voltares ao presente.</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
