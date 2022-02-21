a1d5c0ef15b94f4c686ad10852e6b0a41614927d
David Amaral Silva <david_silva_94@live.com.pt>
2022-02-21 17:37:14 +0000

    update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,14 @@
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+    return (
+        <div>
+        <h1>Pareces estar perdido na Historia</h1>
+            <img src=""  alt="ampulheta erro 404"/>
+            
+            <h3>Clica</h3>
+            
+            <button type="button"> aqui </button>
+             
+            <h3>para voltares ao presente</h3>
+        </div> 
+    )
   }
