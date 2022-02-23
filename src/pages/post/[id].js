17759e77426079e462fbeb30b68f44aa7596891f
David Amaral Silva <david_silva_94@live.com.pt>
2022-02-23 18:58:53 +0000

    id update

--- src/pages/post/[id].js
+++ src/pages/post/[id].js
@@ -35,6 +35,7 @@ export default function Post() {
         <CommentCell key={comments.id} comments={comments}/>
         ))}
       </CommentsContainer>
+      <InsertComment />
     </Transition>
   );
 }
