41ae2cf871529854aab60101d6546e448be2187f
David Amaral Silva <david_silva_94@live.com.pt>
2022-02-23 18:47:01 +0000

    CommentCell / [id]

--- src/components/CommentCell.js
+++ src/components/CommentCell.js
@@ -1,9 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import db from "../../db.json";
 
 export default function CommentCell() {
+
+  /*
+  const comments = db.comments.filter(
+    (comments) => comments.postId === post.id
+  ); */
+  
   return (
     <div className="flex mx-4 w-full">
+      <div
+       /* data={comments}
+        initialNumToRender={10}
+        ListEmptyComponent={() => <EmptyCommentList erro="Sem comentários" />} 
+        renderItem={({ body }) => <CommentCell comment={body} />}
+        keyExtractor={(body) => body.id} */
+      
+
+
+      />
       <img
         className="w-10 h-10 rounded-full mr-4"
         src="https://pbs.twimg.com/profile_images/885868801232961537/b1F6H4KC_400x400.jpg"
