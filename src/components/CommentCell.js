56eb57019f10fd3afd975c6832ad5d2941dc4234
teixeiraj25 <joao.teixeira04@ua.pt>
2022-02-23 16:39:16 +0000

    update comment cell

--- src/components/CommentCell.js
+++ src/components/CommentCell.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function CommentCell() {
   return (
-    <div className="flex mx-4">
+    <div className="flex mx-4 w-full">
       <img
         className="w-10 h-10 rounded-full mr-4"
         src="https://pbs.twimg.com/profile_images/885868801232961537/b1F6H4KC_400x400.jpg"
@@ -12,7 +12,7 @@ export default function CommentCell() {
       <input
           type="text"
           placeholder="Escreve um comentário..."
-          className="bg-gray-300 rounded-full text-sm font-light tracking-wider outline-none"
+          className="w-full py-2 px-5 bg-gray-200 rounded-full text-sm font-light tracking-wider outline-none"
         />
     </div>
   );
