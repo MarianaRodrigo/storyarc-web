4c1e5bd99316c7bd706c1a0aea1cadcdb66bc250
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-02-25 00:48:55 +0000

    updated images domains

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ module.exports = {
       "pk.eyJ1IjoiamFyYmFzNzciLCJhIjoiY2t6bGp2MjY5MmdtdDJub2NmYXR6ZWE3ZiJ9.7tPF-rPpXiE99xWJUVKBbg",
   },
   images: {
-    domains: ["randomuser.me", "pbs.twimg.com", "i.pinimg.com"],
+    domains: [
+      "randomuser.me",
+      "pbs.twimg.com",
+      "i.pinimg.com",
+      "static.globalnoticias.pt",
+      "eduportugal.eu",
+      "ww3.aeje.pt",
+      "api-assets.ua.pt",
+      "reencontrosdemusicacontemporanea.pt",
+      "www.zarllor.com",
+      "www.teatroaveirense.pt",
+    ],
   },
 };
