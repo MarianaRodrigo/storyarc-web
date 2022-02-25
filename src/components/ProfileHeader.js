87aad4a6a90472c5f216894d9267c8cc1156fdcd
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-02-25 20:00:09 +0000

    Profile Header fixed

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import db from '../../db.json'
-import Image from 'next/image'
+import React from "react";
+import db from "../../db.json";
+import Image from "next/image";
 
 export default function ProfileHeader() {
-  const user = db.users[3]
+  const user = db.users[3];
   return (
-    <div className="mt-6 h-64 flex bg-gradient-to-b from-current to-white relative">
-      <Image className="opacity-50" src="/images/profile.png" layout="fill" />
+    <div className="mt-6 h-64 relative">
+      <Image src="/images/profile.png" alt="Moliceiro" layout="fill" />
+      <div className="absolute w-full h-full bg-gradient-to-t from-white to-transparent" />
       <div className="absolute w-24 h-24 top-[30%] left-8">
-        <Image src={user.avatar} layout="fill" className="rounded-full" />
+        <Image
+          src={user.avatar}
+          alt={user.name}
+          layout="fill"
+          className="rounded-full"
+        />
       </div>
     </div>
-  )
+  );
 }
