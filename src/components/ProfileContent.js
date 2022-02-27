e6220b6390110e86c777699449c171cfd14ccbf9
David Amaral Silva <david_silva_94@live.com.pt>
2022-02-27 18:42:40 +0000

    Update ProfileContent.js

--- src/components/ProfileContent.js
+++ src/components/ProfileContent.js
@@ -1,19 +1,20 @@
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+
+import { Tabs, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import Image from "next-image"
 
 export function ProfileContent () {
     return (
   <Tabs>
     <TabPanel>
       <h2>Ver Publicações Próprias </h2>
-      <Image src="/images/Seta.png" alt="Seta" />
+      <img src="/images/Seta.png" alt="Seta" />
       <h2>Ver Publicações que gostei</h2>
-      <Image src="/images/Seta.png" alt="Seta" />
+      <img src="/images/Seta.png" alt="Seta" />
       <h2> Notificações </h2>
-      <imImageg src="/images/Seta.png" alt="Seta" />
+      <img src="/images/Seta.png" alt="Seta" />
       <h2> Sobre Aveiro Storyarc </h2>
-      <Image src="/images/Seta.png" alt="Seta" />
+      <img src="/images/Seta.png" alt="Seta" />
     </TabPanel>
 
     
