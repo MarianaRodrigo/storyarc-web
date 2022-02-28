7eb8e80652117e2dc2fc120ef160285670114f94
David Amaral Silva <david_silva_94@live.com.pt>
2022-02-28 16:03:57 +0000

    Update Profile

--- src/components/ProfileContent.js
+++ src/components/ProfileContent.js
@@ -1,23 +1,15 @@
-
-
-import { Tabs, TabPanel } from 'react-tabs';
+import { Tabs,TabList, Tab } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 export function ProfileContent () {
     return (
   <Tabs>
-    <TabPanel>
-      <h2>Ver Publicações Próprias </h2>
-      <img src="/images/Seta.png" alt="Seta" />
-      <h2>Ver Publicações que gostei</h2>
-      <img src="/images/Seta.png" alt="Seta" />
-      <h2> Notificações </h2>
-      <img src="/images/Seta.png" alt="Seta" />
-      <h2> Sobre Aveiro Storyarc </h2>
-      <img src="/images/Seta.png" alt="Seta" />
-    </TabPanel>
-
-    
+    <TabList>
+      <Tab>Publicações Próprias </Tab>
+      
+      <Tab>Publicações que gostei</Tab>
+     
+    </TabList>
   </Tabs>
     );
 }
