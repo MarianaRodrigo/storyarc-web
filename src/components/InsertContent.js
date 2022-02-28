b95749213df0a649eee816773072aa06f9c649cf
teixeiraj25 <joao.teixeira04@ua.pt>
2022-02-28 16:46:14 +0000

    modal atualizada

--- src/components/InsertContent.js
+++ src/components/InsertContent.js
@@ -71,7 +71,8 @@ export function InsertContent() {
               <div className="shadow appearance-none border rounded w-full py-6 px-3 text-gray-700 font-light tracking-wide leading-tight focus:outline-none focus:shadow-outline">
                 <svg class="w-28 h-28 m-auto rotate-12" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                 <p className="flex justify-center mb-6 text-sm">Adiciona uma fotografia</p>
-                <button type="button" className="flex justify-center m-auto text-white bg-verde focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Selecionar no computador</button>
+                <label for="myfile">Selecionar</label>
+                <input type="file" id="myfile" name="myfile" className="flex justify-center m-auto text-white bg-verde focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"></input>
               </div>
             </div>
 
