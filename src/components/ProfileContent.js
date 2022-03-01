f17e6fc02b6fd7b351c6b768d9c6c54eca0f5b78
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-03-01 15:23:49 +0000

    updated styling

--- src/components/ProfileContent.js
+++ src/components/ProfileContent.js
@@ -1,13 +1,33 @@
-import { Tabs,TabList, Tab } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import { useState } from "react";
+import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { useSelector } from "react-redux";
+import { useUser } from "../features/user/userSlice";
+import { useGetUserPostsQuery } from "../services/storyarc";
+import { Card } from "../components";
 
-export function ProfileContent () {
-    return (
-  <Tabs>
-    <TabList className="flex  flex-grow items-center">
-      <Tab> Publicações Próprias </Tab>
-      <Tab> Publicações que gostei </Tab>
-    </TabList>
-  </Tabs>
-    );
+export function ProfileContent() {
+  const user = useSelector(useUser);
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const { data, isFetching, isLoading } = useGetUserPostsQuery({
+    uid: user.id,
+  });
+
+  console.log(user);
+  return (
+    <Tabs
+      selectedIndex={tabIndex}
+      onSelect={(index) => setTabIndex(index)}
+      className="my-2 py-2"
+      selectedTabClassName="bg-black text-white rounded"
+    >
+      <TabList className="flex justify-center">
+        <Tab>Publicações próprias</Tab>
+        <Tab>Favoritos</Tab>
+      </TabList>
+      <TabPanel>dasd</TabPanel>
+      <TabPanel>banana</TabPanel>
+    </Tabs>
+  );
 }
