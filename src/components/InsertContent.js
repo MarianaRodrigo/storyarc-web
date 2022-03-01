02738b3476c6823c653fb88fb8eef1ee1d1d54ae
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-03-01 15:29:18 +0000

    chanded class to className

--- src/components/InsertContent.js
+++ src/components/InsertContent.js
@@ -64,19 +64,40 @@ export function InsertContent() {
                 </svg>
               </button>
             </div>
-            
+
             <div className="p-6 space-y-6">
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 font-light tracking-wide leading-tight focus:outline-none focus:shadow-outline pb-12" type="text" placeholder="Adiciona uma descrição"></input>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 font-light tracking-wide leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Introduz a localização da fotografia"></input>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 font-light tracking-wide leading-tight focus:outline-none focus:shadow-outline" type="date" placeholder="Seleciona uma data"></input>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 font-light tracking-wide leading-tight focus:outline-none focus:shadow-outline pb-12"
+                type="text"
+                placeholder="Adiciona uma descrição"
+              ></input>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 font-light tracking-wide leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Introduz a localização da fotografia"
+              ></input>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 font-light tracking-wide leading-tight focus:outline-none focus:shadow-outline"
+                type="date"
+                placeholder="Seleciona uma data"
+              ></input>
               <Photo />
             </div>
-           
-            
 
             <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200">
-              <button type="button" className="text-white bg-verde focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Adicionar</button>
-              <button onClick={() => dispatch(setAddContent(false))} type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Cancelar</button>
+              <button
+                type="button"
+                className="text-white bg-verde focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Adicionar
+              </button>
+              <button
+                onClick={() => dispatch(setAddContent(false))}
+                type="button"
+                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
+              >
+                Cancelar
+              </button>
             </div>
           </div>
         </div>
