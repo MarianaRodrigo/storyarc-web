8073b1dafa95c2db0dd5c9af86ecd8437f034507
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-03-01 20:21:57 +0000

    hide addbutton when there's no user

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import { InsertContent } from "./";
 import { useSelector, useDispatch } from "react-redux";
+import { useUser } from "../features/user/userSlice";
 import {
   setAddContent,
   isShowingContentModal,
@@ -10,6 +11,7 @@ import {
 
 export function SubHeader() {
   const isShowing = useSelector(isShowingContentModal);
+  const user = useSelector(useUser);
   const dispatch = useDispatch();
 
   return (
@@ -24,18 +26,30 @@ export function SubHeader() {
       leaveTo="opacity-0 "
       className="flex mx-4 justify-between items-center my-6"
     >
-      <div className="flex flex-grow items-center">
+      <div className="flex justify-between items-center">
         <h1 className=" text-xl font-normal tracking-wide">
           Conteúdos Recentes
         </h1>
       </div>
-      <FontAwesomeIcon
-        onClick={() => dispatch(setAddContent(true))}
-        className="w-8 h-8 hover:scale-125 transition duration-300 ease-out cursor-pointer"
-        data-modal-toggle="defaultModal"
-        style={{ color: "#37b780" }}
-        icon={faCirclePlus}
-      />
+
+      <Transition
+        show={user ? true : false}
+        enter="transition ease duration-200 transform"
+        enterFrom="opacity-0 scale-0"
+        enterTo="opacity-100 scale-1"
+        leave="transition ease duration-200 transform"
+        leaveFrom="opacity-100 scale-1"
+        leaveTo="opacity-0 scale-0"
+      >
+        <FontAwesomeIcon
+          onClick={() => dispatch(setAddContent(true))}
+          className="w-8 h-8 hover:scale-125 transition duration-300 ease-out cursor-pointer"
+          data-modal-toggle="defaultModal"
+          style={{ color: "#37b780" }}
+          icon={faCirclePlus}
+        />
+      </Transition>
+
       {isShowing && <InsertContent />}
     </Transition>
   );
